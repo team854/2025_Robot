a376c7e6f3997cd6ef7a879321a4be525bf0566c
Jonathan Graydon <85715209+Romgi@users.noreply.github.com>
2025-02-01 11:44:20 -0500

    updated module configs and tuned swerve base

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,8 +20,8 @@ public final class Constants {
         public static final int    kDriverControllerPort    = 0;
         public static final double DEADBAND                 = 0.1;
         public static final double SWERVE_TRANSLATION_SCALE = 1;
-        public static final double SWERVE_ROTATION_SCALE    = -2; // Negative values invert right stick
+        public static final double SWERVE_ROTATION_SCALE    = 1.2; // Negative values invert right stick
     }
 
-    public static final double MAX_SPEED = Units.feetToMeters(24);
+    public static final double MAX_SPEED = Units.feetToMeters(6);
 }
