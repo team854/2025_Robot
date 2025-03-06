011d62c8b7f1507a5edbdf0d65c7613f7639f935
Jonathan Graydon <85715209+Romgi@users.noreply.github.com>
2025-03-06 10:06:37 -0500

    Finalized PathPlanner Config

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -220,7 +220,6 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        // An example command will be run in autonomous
         return autoModeChooser.getSelectedAutoCommand();
     }
 }
