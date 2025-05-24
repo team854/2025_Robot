acad0a08e3c943a996f233510acf009f0893ae7c
Jonathan Graydon <85715209+Romgi@users.noreply.github.com>
2025-05-24 15:18:32 -0400

    1310 event final

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,8 +20,8 @@ public final class Constants {
         public static final int    kDriverControllerPort    = 0;
         public static final int    kOperatorControllerPort  = 1;
         public static final double DEADBAND                 = 0.04;
-        public static final double SWERVE_TRANSLATION_SCALE = 0.75;
-        public static final double SWERVE_ROTATION_SCALE    = 0.5;                     // Negative values invert right stick
+        public static final double SWERVE_TRANSLATION_SCALE = 1.0;
+        public static final double SWERVE_ROTATION_SCALE    = 0.6;                     // Negative values invert right stick
         public static final double MAX_SPEED                = Units.feetToMeters(14.5);
         public static final double FINE_SPEED_REDUCTION     = Units.feetToMeters(2);
         public static final double FINE_ROTATION_REDUCTION  = 0.8;
